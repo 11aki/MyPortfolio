84a5e5d541ab6dd4da7bebde667233c67dd75bdf
Sylvia <littleverything8@gmail.com>
2021-02-11 00:45:12 +1100

    buggy drop down table

---
+++ website/static/js/collapsetable.js
@@ -0,0 +1,14 @@
+$('.collapse').on('show.bs.collapse', function () {
+    var groupId = $('#expander').attr('data-group-id');
+    if (groupId) {
+      $('#grandparentIcon').html('v');
+    }
+  });
+  
+  $('.collapse').on('hide.bs.collapse', function () {
+    var groupId = $('#expander').attr('data-group-id');
+    if (groupId) {
+      $('#' + groupId + 'Icon').html('>');
+    }
+  });
+  
